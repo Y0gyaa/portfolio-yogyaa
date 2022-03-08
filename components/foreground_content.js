4e0e0b98bb830b516ece722f63de9cb3d67f9923
Yogyaa Varma <yogyaa.varma@gmail.com>
2022-03-08 19:46:05 +0530

    💄 Layout fixes-initial

---
+++ components/foreground_content.js
@@ -0,0 +1,15 @@
+export default function Foreground_content() {
+
+    return (
+        <div className="full-page">
+            <div className="head-position">
+                <span className="name-head">YOGYAA VARMA</span>
+                <div className="title-head">
+                    <span className="second-head"> FRONTEND</span>
+                    <br />
+                    <span className="second-head">DEVELOPER</span>
+                </div>
+            </div>
+        </div>
+    )
+};
--- components/particle_background.js
+++ components/particle_background.js
@@ -2,95 +2,95 @@ import Particles from "react-tsparticles";
 
 export default function Particle_background() {
   const particlesInit = (main) => {
-    
+
   };
 
   const particlesLoaded = (container) => {
-    
+
   };
   return (
-    <div>
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: "#000000",
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            // onClick: {
-            //   enable: true,
-            //   mode: "push",
-            // },
-            onHover: {
-              enable: true,
-              mode: "repulse",
+    <div className="particle-background">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#000000",
             },
-            resize: true,
           },
-          modes: {
-            bubble: {
-              distance: 40,
-              duration: 0.2,
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              // onClick: {
+              //   enable: true,
+              //   mode: "push",
+              // },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 40,
+                duration: 0.2,
+                opacity: 1,
+                size: 40,
+              },
+              push: {
+                quantity: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#faed27",
+            },
+            links: {
+              color: "#39FF14",
+              distance: 150,
+              enable: true,
               opacity: 1,
-              size: 40,
+              width: 1,
             },
-            push: {
-              quantity: 3,
+            collisions: {
+              enable: false,
             },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#faed27",
-          },
-          links: {
-            color: "#39FF14",
-            distance: 150,
-            enable: true,
-            opacity: 1,
-            width: 1,
-          },
-          collisions: {
-            enable: false,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: true,
-            speed: 5,
-            straight: true,
-          },
-          number: {
-            density: {
+            move: {
+              direction: "none",
               enable: true,
-              area: 800,
+              outMode: "bounce",
+              random: true,
+              speed: 5,
+              straight: true,
             },
-            value: 50,
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.6,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
           },
-          opacity: {
-            value: 0.6,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: false,
-      }}
-    /></div>
+          detectRetina: false,
+        }}
+      /></div>
   )
 }
