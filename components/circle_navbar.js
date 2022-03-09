4358b3d49ce6e60cc2d11b4aa0233d9975cd56a9
Yogyaa Varma <yogyaa.varma@gmail.com>
2022-03-09 20:34:10 +0530

    💄 Layout update

---
+++ components/circle_navbar.js
@@ -0,0 +1,24 @@
+export default function Circle_navbar() {
+
+    return (
+        <div className="circles-bar">
+            <svg width="100" height="100">
+                <circle cx="60" cy="60" r="40"
+                    fill="#faed27" />
+            </svg>
+            <img className="circle-png" src="/media/github_icon.png" />
+            <br />
+            <svg width="100" height="100">
+                <circle cx="60" cy="60" r="40"
+                    fill="#faed27" />
+            </svg>
+            <img className="circle-png" src="/media/linkedin_icon.png" />
+            <br />
+            <svg width="100" height="100">
+                <circle cx="60" cy="60" r="40"
+                    fill="#faed27" />
+            </svg>
+            <img className="circle-png" src="/media/save_cv_icon.png" />
+        </div>
+    )
+};
---
+++ components/menu_bar.js
@@ -0,0 +1,12 @@
+export default function Menu_bar() {
+
+    return (
+        <div className="menu-box">
+            <span className="menu-text">|  PORTFOLIO  |</span>
+            <br />
+            <span className="menu-text">|  MY_DEV_JOURNEY  |</span>
+            <br />
+            <span className="menu-text">|  RESUME  |</span>
+        </div>
+    )
+};
